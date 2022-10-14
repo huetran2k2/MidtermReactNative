0e5db0ab432ad86d4b618ef6def0c8ce117e16a4
huetran2k2 <100479860+huetran2k2@users.noreply.github.com>
2022-10-14 17:22:15 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, TouchableOpacity} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import FirstScreen from './components/direction';
+import SecondScreen from './components/position';
+import ThirdScreen from './components/alignItems';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="direction" component={FirstScreen} options={{ tabBarBadge: 1 }} />
+      <Tab.Screen name="position" component={SecondScreen} options={{ tabBarBadge: 2 }} />
+      <Tab.Screen name="alignItems" component={ThirdScreen} options={{ tabBarBadge: 3 }} />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
